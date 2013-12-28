857dee860e5a0f601ea2a68b0c845497d547ef38
ajauhri <abhinavjauhri@gmail.com>
2013-12-28 00:55:23 -0800

    adding missing point tc1

--- luas/tc1_ex.lua
+++ luas/tc1_ex.lua
@@ -1,5 +1,5 @@
-load_platform("S1 - 4ft square plate fixed.nec")
-add_antenna("A2 - 150mono fixed.nec")
+load_platform("input/S1 - 4ft square plate fixed.nec")
+add_antenna("input/A2 - 150mono fixed.nec")
 add_point(  0.101600,   0.000000,   0.000000)
 add_point(  0.203200,   0.000000,   0.000000)
 add_point(  0.304800,   0.000000,   0.000000)
@@ -83,7 +83,8 @@ add_point(  0.203200,  -0.508000,   0.000000)
 add_point(  0.304800,  -0.508000,   0.000000)
 add_point(  0.304800,  -0.609600,   0.000000)
 add_point(  0.406400,  -0.609600,   0.000000)
-add_antenna("A5 - 300dipole fixed.nec")
+add_point( -0.508000,  -0.101600,   0.000000)
+add_antenna("input/A5 - 300dipole fixed.nec")
 add_point( -0.609600,   0.609600,   0.000000)
 add_point( -0.508000,   0.609600,   0.000000)
 add_point( -0.406400,   0.609600,   0.000000)
@@ -167,11 +168,12 @@ add_point( -0.406400,   0.000000,   0.000000)
 add_point( -0.304800,   0.000000,   0.000000)
 add_point( -0.203200,   0.000000,   0.000000)
 add_point( -0.101600,   0.000000,   0.000000)
+add_point( -0.406400,   0.406400,   0.000000)
 params = {
     mutation = 0.0,
     exp_weight = 2,
     algorithm = "EX",
-    run_simulator = 0,
+    run_simulator = 1,
     max_gain = 1,
     max_coup = 1,
     min_coup = 1,
